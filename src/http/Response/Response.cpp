b9e333eb0f9a38e6245ce52f368aa420a9269bf8
justJuste <jules.longin1@gmail.com>
2025-10-07 23:20:32 +0200

    conpile Linux

--- src/http/Response/Response.cpp
+++ src/http/Response/Response.cpp
@@ -187,7 +187,13 @@ void	Response::_handlePost( const std::string & path )
 	std::string expect = _req->getHeader("Expect");
 	std::string cl = _req->getHeader("Content-Length");
 	size_t maxSize = _server->getMaxSize();
-	if (expect == "100-continue" || (!cl.empty() && std::stoul(cl) > maxSize))
+	unsigned long contentLength = 0;
+	if (!cl.empty())
+	{
+		std::stringstream ss(cl);
+		ss >> contentLength;
+	}
+	if (expect == "100-continue" || (!cl.empty() && contentLength > maxSize))
 	{
 		std::stringstream ss;
 		ss << "POST request has a content too large: > " << rounded(_server->getMaxSize());
