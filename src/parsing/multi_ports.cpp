f777bf86e93e25cec28182252ec5cc3807e86494
justJuste <jules.longin1@gmail.com>
2025-10-09 04:55:17 +0200

    v1.2.20 -- concat

--- src/parsing/multi_ports.cpp
+++ src/parsing/multi_ports.cpp
@@ -28,9 +28,7 @@ std::vector<Server *>	multi_ports( std::vector<Server *> & servers )
 		firstPort.push_back(ports[0]);
 		servers[i]->setPort(firstPort);
 	}
-
-	std::vector<Server *>::iterator it = res.begin();
-	for (; it != res.end(); ++it)
+	for (std::vector<Server *>::iterator it = res.begin(); it != res.end(); ++it)
 		for (std::vector<Server *>::iterator cit = it + 1; cit != res.end(); ++cit)
 			if ((*it)->getHost() == (*cit)->getHost() && (*it)->getPort() == (*cit)->getPort())
 				(*cit)->setDefault(false);
